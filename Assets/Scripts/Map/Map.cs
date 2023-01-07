be65f3e0e3f48c0216754585779df6a9e04dac59
Alexandre Touchette <alexandre.touchette@polymtl.ca>
2023-01-07 11:39:46 -0500

    Ajout du character et de la map de base

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,115 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public BiomePreset[] biomes;
+    public GameObject tilePrefab;
+    public Transform parent;
+
+    [Header("Dimensions")]
+    public int width = 50;
+    public int height = 50;
+    public float scale = 1.0f;
+    public Vector2 offset;
+
+    [Header("Height Map")]
+    public Wave[] heightWaves;
+    public Gradient heightDebugColors;
+    public float[,] heightMap;
+
+    [Header("Moisture Map")]
+    public Wave[] moistureWaves;
+    public Gradient moistureDebugColors;
+    private float[,] moistureMap;
+
+    [Header("Heat Map")]
+    public Wave[] heatWaves;
+    public Gradient heatDebugColors;
+    private float[,] heatMap;
+
+    private float last;
+
+    void Start ()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap ()
+    {
+        // height map
+        heightMap = NoiseGenerator.Generate(width, height, scale, heightWaves, offset);
+
+        // moisture map
+        moistureMap = NoiseGenerator.Generate(width, height, scale, moistureWaves, offset);
+
+        // heat map
+        heatMap = NoiseGenerator.Generate(width, height, scale, heatWaves, offset);
+
+        int i = 0;
+        Color[] pixels = new Color[width * height];
+
+        for(int x = 0; x < width; ++x)
+        {
+            for(int y = 0; y < height; ++y)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity, parent);
+                tile.GetComponent<SpriteRenderer>().sprite = GetBiome(heightMap[x, y], moistureMap[x, y], heatMap[x, y]).GetTleSprite();
+            }
+        }
+    }
+
+    public class BiomeTempData
+    {
+        public BiomePreset biome;
+
+        public BiomeTempData (BiomePreset preset)
+        {
+            biome = preset;
+        }
+        
+        public float GetDiffValue (float height, float moisture, float heat)
+        {
+            return (height - biome.minHeight) + (moisture - biome.minMoisture) + (heat - biome.minHeat);
+        }
+    }
+
+    BiomePreset GetBiome (float height, float moisture, float heat)
+    {
+        BiomePreset biomeToReturn = null;
+        List<BiomeTempData> biomeTemp = new List<BiomeTempData>();
+
+        foreach(BiomePreset biome in biomes)
+        {
+            if(biome.MatchCondition(height, moisture, heat))
+            {
+                biomeTemp.Add(new BiomeTempData(biome));
+            }
+        }
+
+        float curVal = 0.0f;
+
+        foreach(BiomeTempData biome in biomeTemp)
+        {
+            if(biomeToReturn == null)
+            {
+                biomeToReturn = biome.biome;
+                curVal = biome.GetDiffValue(height, moisture, heat);
+            }
+            else
+            {
+                if(biome.GetDiffValue(height, moisture, heat) < curVal)
+                {
+                    biomeToReturn = biome.biome;
+                    curVal = biome.GetDiffValue(height, moisture, heat);
+                }
+            }
+        }
+
+        if(biomeToReturn == null)
+            biomeToReturn = biomes[0];
+
+        return biomeToReturn;
+    }
+}
