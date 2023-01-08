89036968978a239fbea20e58cae0619ce1b7b902
BuildTools <alexandre.touchette@outlook.com>
2023-01-08 13:06:27 -0500

    Images working

--- Assets/Scripts/CardData.cs
+++ Assets/Scripts/CardData.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum CardType
 {
@@ -17,6 +18,7 @@ public struct CardData
     public CardType cardType { get; set; }
     public string title { get; set; }
     public string description { get; set; }
+    public Sprite image { get; set; }
     public int numToCraft { get; set; }
     public ItemType itemType { get; set; }
 }
